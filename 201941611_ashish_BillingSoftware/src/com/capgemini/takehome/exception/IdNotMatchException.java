05cf87cfe145691924ce9e688ebcc2c1f4d38f32
ashishbhatia123 <35377413+ashishbhatia123@users.noreply.github.com>
2019-02-25 10:30:33 +0530

    Delete IdNotMatchException.java

--- 201941611_ashish_BillingSoftware/src/com/capgemini/takehome/exception/IdNotMatchException.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.takehome.exception;
-
-public class IdNotMatchException extends Exception {
-	public IdNotMatchException(int productCode)
-	{
-		System.out.println(" Sorry ! the product code <<"+ productCode+ ">> is not available");
-	}
-
-}
